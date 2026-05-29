e13991916b7fad41613a7effa086fd1bc46a6e5a
lucaslkds <91992988+lucaslkds@users.noreply.github.com>
2026-05-29 17:14:35 -0300

    Add files via upload

--- src/sistema/ConsoleService.java
+++ src/sistema/ConsoleService.java
@@ -7,7 +7,7 @@ import java.nio.charset.StandardCharsets;
 
 public class ConsoleService {
 
-    private ConsoleService() {
+    public ConsoleService() {
     }
 
     public static void configurarUtf8() {
